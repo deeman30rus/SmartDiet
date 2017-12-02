a95f5f5ba28dc3f7a68042d20c70d544fb01f53d
Kuzma <deeman30rus@gmail.com>
2017-12-02 23:08:18 +0400

    модель рецепта

---
+++ app/src/main/java/com/delizarov/smartdiet/domain/models/Unit.java
@@ -0,0 +1,13 @@
+package com.delizarov.smartdiet.domain.models;
+
+
+public enum Unit {
+
+    Kilo, // кг
+    Gram, // г
+    Liter,
+    TeaSpoon, // ч. ложка
+    TableSpoon, // ст. ложка
+    Pich, // щепоть
+    Piece, // шт.
+}
