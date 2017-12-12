cf69e79001c4e418332db111ef0f1c29f6a9fd06
Kuzma <deeman30rus@gmail.com>
2017-12-12 07:17:18 +0400

    карточка рецепта

--- app/src/main/java/com/delizarov/smartdiet/ui/viewholders/RecipeViewHolder.java
+++ app/src/main/java/com/delizarov/smartdiet/ui/viewholders/RecipeViewHolder.java
@@ -4,10 +4,13 @@ package com.delizarov.smartdiet.ui.viewholders;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.delizarov.smartdiet.R;
 import com.delizarov.smartdiet.domain.models.Recipe;
+
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -16,6 +19,18 @@ public class RecipeViewHolder extends ViewHolderBase<Recipe> {
 
     @BindView(R.id.title)
     TextView title;
+
+    @BindView(R.id.recipe_pic)
+    ImageView recipePic;
+
+    @BindView(R.id.description)
+    TextView description;
+
+    @BindView(R.id.cook_time)
+    TextView cookTime;
+
+    @BindView(R.id.calories)
+    TextView calories;
 
     public RecipeViewHolder(int layoutResource, final ViewGroup parent) {
 
@@ -28,5 +43,28 @@ public class RecipeViewHolder extends ViewHolderBase<Recipe> {
     public void bind(Recipe recipe) {
 
         title.setText(recipe.getTitle());
+
+        recipePic.setImageResource(R.drawable.pancakes_pic);
+
+        description.setText(recipe.getDescription());
+
+        cookTime.setText(minutesText(recipe.getCookTime()));
+
+        calories.setText(String.format(Locale.getDefault(), "%d ккал", recipe.getCookTime()));
+    }
+
+    private static String minutesText(int minutes) {
+
+        if (minutes == 1)
+            return "1 минута";
+
+        int r10 = minutes % 10;
+        int r100 = minutes & 100;
+
+        if (r100 >= 5 && r100 < 20)
+            return String.format(Locale.getDefault(), "%d минут", minutes);
+
+        return String.format(Locale.getDefault(), "%d %s", minutes, (r10 < 5 ? "минуты" : "минут"));
+
     }
 }
