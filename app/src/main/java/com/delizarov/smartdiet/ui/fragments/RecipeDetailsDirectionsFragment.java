6534ebc58d565e81e209b659e5c75a8fc39501ad
Kuzma <deeman30rus@gmail.com>
2017-12-17 18:48:55 +0400

    детали рецепта способ приготовления

--- app/src/main/java/com/delizarov/smartdiet/ui/fragments/RecipeDetailsDirectionsFragment.java
+++ app/src/main/java/com/delizarov/smartdiet/ui/fragments/RecipeDetailsDirectionsFragment.java
@@ -3,18 +3,48 @@ package com.delizarov.smartdiet.ui.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.delizarov.smartdiet.R;
+import com.delizarov.smartdiet.domain.models.Recipe;
+import com.delizarov.smartdiet.ui.adapters.SortedListAdapter;
+import com.delizarov.smartdiet.ui.viewholders.RecipeDirectionViewHolder;
+import com.delizarov.smartdiet.ui.viewholders.ViewHolderBase;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 public class RecipeDetailsDirectionsFragment extends RecipeDetailsFragment {
+
+    @BindView(R.id.recipe_directions)
+    RecyclerView directions;
 
     public static RecipeDetailsDirectionsFragment newInstance() {
 
         return new RecipeDetailsDirectionsFragment();
     }
+
+    private SortedListAdapter<Recipe.Direction> mAdapter = new SortedListAdapter<Recipe.Direction>(Recipe.Direction.class, (d1, d2) -> Integer.compare(d1.Order, d2.Order)) {
+        @Override
+        public ViewHolderBase<Recipe.Direction> onCreateViewHolder(ViewGroup parent, int viewType) {
+
+            RecipeDirectionViewHolder viewHolder = new RecipeDirectionViewHolder(R.layout.vh_recipe_direction, parent);
+
+            return viewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolderBase<Recipe.Direction> holder, int position) {
+
+            holder.bind(get(position));
+        }
+    };
+
+    private Unbinder mUnbinder;
 
     @Nullable
     @Override
@@ -22,6 +52,19 @@ public class RecipeDetailsDirectionsFragment extends RecipeDetailsFragment {
 
         View view = inflater.inflate(R.layout.fragment_recipe_details_directions, container, false);
 
+        mUnbinder = ButterKnife.bind(this, view);
+
+        directions.setAdapter(mAdapter);
+
+        mAdapter.addAll(mRecipe.getDirections());
+
         return view;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        mUnbinder.unbind();
     }
 }
